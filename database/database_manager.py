323dc9fc59f73295d922b505ea27398290317631
Milad Natanzi <miladnatanzi@gmail.com>
2024-08-01 00:26:17 -0400

    FYI

--- database/database_manager.py
+++ database/database_manager.py
@@ -12,7 +12,6 @@ from influxdb_client.client.delete_api import DeleteApi
 from influxdb_client.client.write_api import SYNCHRONOUS
 from logs.logger_config import database_logger  # Import the configured logger
 from datetime import datetime
-from influxdb_client import Point
 import json
 import threading
 
@@ -27,32 +26,24 @@ INFLUXDB_BUCKET = os.getenv('INFLUXDB_BUCKET', 'RAN_metrics')
 class DatabaseManager:
     _instance = None
     _lock = threading.Lock()  # Add a class-level lock
-    _call_count = 0  # Add a class variable to count calls database manager instance
 
     @classmethod
     def get_instance(cls):
-        cls._call_count += 1
-        database_logger.debug(f"DatabaseManager get_instance called {cls._call_count} times.")
         if cls._instance is None:
             with cls._lock:
                 if cls._instance is None:
-                    database_logger.debug("Creating a new instance of DatabaseManager.")
                     cls._instance = cls()
                     cls._instance.client_init()
-                else:
-                    database_logger.debug("Returning existing instance of DatabaseManager.")
         return cls._instance
 
     def client_init(self):
         # This method replaces the original __init__ content
-        #print(f"InfluxDB token: {INFLUXDB_TOKEN}")  # Print the InfluxDB token for test
-        #print(f"Connecting to InfluxDB with URL: {INFLUXDB_URL}, Token: {INFLUXDB_TOKEN}, Org: {INFLUXDB_ORG}")
         self.client = InfluxDBClient(url=INFLUXDB_URL, token=INFLUXDB_TOKEN, org=INFLUXDB_ORG)
         self.write_api = self.client.write_api(write_options=SYNCHRONOUS)
         self.query_api = self.client.query_api()
         self.bucket = INFLUXDB_BUCKET
         self.org = INFLUXDB_ORG
-##################################################################################################################################
+
     def get_sector_by_id(self, sector_id):
         query = f'from(bucket: "{self.bucket}") |> range(start: -1d) |> filter(fn: (r) => r._measurement == "sector_metrics" and r.sector_id == "{sector_id}")'
         result = self.query_api.query(query=query)
@@ -61,12 +52,12 @@ class DatabaseManager:
                 if record.values.get('sector_id') == sector_id:
                     return record.values  # Adjust based on what you need
         return None
-################################################################################################################################
+
     def insert_sector_state(self, sector):
         """Inserts the state of a sector into InfluxDB."""
         point = sector.serialize_for_influxdb()  # Assuming serialize_for_influxdb() prepares the data correctly
         self.insert_data(point)
-##################################################################################################################################
+
     def get_sectors(self):
         try:
             query = f'from(bucket:"{self.bucket}") |> range(start: -30d) |> filter(fn:(r) => r._measurement == "sectors")'
@@ -80,7 +71,7 @@ class DatabaseManager:
         except Exception as e:
             print(f"Failed to retrieve sectors: {e}")
             return []
-##################################################################################################################################
+
     def remove_ue_state(self, ue_id, sector_id):
         """
         Removes the state of a UE from InfluxDB within the last 24 hours.
@@ -101,7 +92,7 @@ class DatabaseManager:
             database_logger.info(f"Successfully removed state for UE {ue_id} in sector {sector_id}.")
         except Exception as e:
             database_logger.error(f"Failed to remove state for UE {ue_id} in sector {sector_id}: {e}")
-##################################################################################################################################
+
     def test_connection(self):
         """Test if the connection to the database is successful."""
         try:
@@ -111,59 +102,44 @@ class DatabaseManager:
         except Exception as e:
             database_logger.error(f"Database connection test failed: {e}")
             return False
-##################################################################################################################################
+
     def insert_data_batch(self, points):
         """Inserts a batch of Point objects into InfluxDB."""
         try:
             self.write_api.write(bucket=self.bucket, record=points)
-            #database_logger.info("Batch data inserted into InfluxDB")
         except Exception as e:
             database_logger.error(f"Failed to insert batch data into InfluxDB: {e}")
-##################################################################################################################################
+
     def insert_data(self, measurement_or_point, tags=None, fields=None, timestamp=None):
-        #print('------------------inside  insert_data Function od database_manager.py -------------------')
         """Inserts or updates data into InfluxDB. Can handle both Point objects and separate parameters."""
         try:
             if isinstance(measurement_or_point, Point):
                 point = measurement_or_point
-                # For debugging: print throughput if it's a field in the Point object
                 if 'throughput' in point._fields:
                     point._fields['throughput'] = float(point._fields['throughput'])
-
-                    #print(f"Throughput (Point object): {throughput_value}, Type: {type(throughput_value)}")
             else:
-                # If separate parameters are provided, create a new Point
                 measurement = measurement_or_point
                 point = Point(measurement)
-                # For debugging: specifically check and print throughput and its type
                 if fields and 'throughput' in fields:
                     fields['throughput'] = float(fields['throughput'])
-                    #print(f"Throughput (field): {throughput_value}, Type: {type(throughput_value)}")
-                # Add tags and fields to the Point
                 for tag_key, tag_value in (tags or {}).items():
                     point.tag(tag_key, tag_value)
                 for field_key, field_value in (fields or {}).items():
                     point.field(field_key, field_value)
-                # Use the provided timestamp or the current time
                 if timestamp is None:
                     timestamp = datetime.utcnow()
                 point.time(timestamp, WritePrecision.S)
-            
-            # Write the point to the database
             self.write_api.write(bucket=self.bucket, record=point)
-            #print('Data write is done')
-
         except Exception as e:
             print(f"Failed to insert data into InfluxDB: {e}")
 
-##################################################################################################################################
     def close_connection(self):
         """Closes the database connection."""
         try:
             self.client.close()
         except Exception as e:
             print(f"Failed to close database connection: {e}")
-##################################################################################################################################
+
     def get_all_ue_ids(self):
         """Retrieves all UE IDs from InfluxDB."""
         try:
@@ -178,7 +154,7 @@ class DatabaseManager:
             database_logger.error(f"Failed to retrieve UE IDs from InfluxDB: {e}")
             return []  # Return an empty list in case of any exception
         return ue_ids
-##################################################################################################################################
+
     def insert_log(self, log_point):
         """Inserts log data into the logs bucket in InfluxDB."""
         log_bucket = 'RAN_logs'
@@ -187,7 +163,7 @@ class DatabaseManager:
             database_logger.info(f"Log data inserted into bucket {log_bucket}")
         except Exception as e:
             database_logger.error(f"Failed to insert log data into InfluxDB: {e}")
-##################################################################################################################################
+
     def update_ue_association(self, ue_id, new_cell_id):
         """
         Updates the association of a UE with a new cell in the database by writing a new point.
@@ -195,19 +171,17 @@ class DatabaseManager:
         :param new_cell_id: The ID of the new cell to associate the UE with.
         """
         try:
-            # Create a new Point with the measurement name you're using for UEs
             point = Point("ue_metrics")\
                 .tag("ue_id", str(ue_id))\
                 .tag("connected_cell_id", str(new_cell_id)) \
                 .field("update_type", "cell_association_change")\
                 .time(datetime.utcnow())
-            # Write the point to InfluxDB
             self.write_api.write(bucket=self.bucket, record=point)
             database_logger.info(f"UE {ue_id} association updated to cell {new_cell_id}")
         except Exception as e:
             database_logger.error(f"Failed to update UE association in the database: {e}")
             raise
-##################################################################################################################################
+
     def write_sector_load(self, sector_id, load):
         point = Point("sector_metrics") \
             .tag("sector_id", sector_id) \
@@ -222,7 +196,6 @@ class DatabaseManager:
             .time(datetime.utcnow(), WritePrecision.S)
         self.write_api.write(bucket=self.bucket, record=point)
 
-###################################################################################################################################
     def write_network_measurement(self, network_load, network_delay, total_handover_success_count, total_handover_failure_count):
         point = Point("network_metrics") \
             .field("network_load", float(network_load)) \
@@ -231,35 +204,26 @@ class DatabaseManager:
             .field("total_handover_failure_count", int(total_handover_failure_count)) \
             .time(datetime.utcnow(), WritePrecision.NS)
         self.write_api.write(bucket=self.bucket, record=point)
-##################################################################################################################################
+
     def get_ue_metrics(self, ue_id):
         print(f"Attempting to fetch UE metrics for ue_id: {ue_id}")  # Debug message 1
-
-        # Define the Flux query to retrieve UE metrics from InfluxDB
         query = f'''
             from(bucket: "{self.bucket}")
                 |> range(start: -1d)
                 |> filter(fn: (r) => r._measurement == "ue_metrics" and r.ue_id == "{ue_id}")
                 |> pivot(rowKey:["_time"], columnKey: ["_field"], valueColumn: "_value")
         '''
-
-        # Execute the query
         result = self.query_api.query(query=query)
-        #print(f'Result of the query inside the get_ue_metrics for ue_id {ue_id}:', result)  # Debug message 2
-
-        # Initialize an empty list to store the metrics
+        print(f'Result of the query inside the get_ue_metrics for ue_id {ue_id}:', result)  # Debug message 2
         metrics = []
-
-        # Check if the query returned any results
         if result:
             for table in result:
-                #print('--------inside for loop DB Manager----table:', table)
+                print('--------inside for loop DB Manager----table:', table)
                 for record in table.records:
-                    #print(f'Record from table: {record}')  # Debug message 3
-                    #print('----DB Manager-------record:', record)
-                    #print('--------------throughput:', record.values.get('throughput', None))
-                    #print('-------------------time:', record.get_time())
-                    # Append the metrics to the list, ensuring safe access to fields
+                    print(f'Record from table: {record}')  # Debug message 3
+                    print('----DB Manager-------record:', record)
+                    print('--------------throughput:', record.values.get('throughput', None))
+                    print('-------------------time:', record.get_time())
                     metrics.append({
                         'timestamp': record.get_time(),
                         'throughput': record.values.get('throughput', None),
@@ -269,29 +233,25 @@ class DatabaseManager:
                     })
         else:
             print("No results found for the query.")
-
-        # Return the list of metrics
         return metrics
-##################################################################################################################################
+
     def get_sector_load(self, sector_id):
-        # Flux query syntax for InfluxDB 2.x
         query = f'''
         from(bucket: "{self.bucket}")
             |> range(start: -1d)
             |> filter(fn: (r) => r["_measurement"] == "sector" and r["sector_id"] == "{sector_id}")
-            |> filter(fn: (r) => r["_field"] == "load")  # Assuming 'load' is a field in your data
+            |> filter(fn: (r) => r["_field"] == "load")
         '''
         result = self.query_api.query(query=query)
         load_metrics = []
         for table in result:
             for record in table.records:
-                # Adjust according to the structure of your data
                 load_metrics.append({
                     'load': record.get_value(),
                     'time': record.get_time()
                 })
         return load_metrics
-##################################################################################################################################
+
     def flush_all_data(self):
         from datetime import datetime, timezone
         import requests
@@ -302,8 +262,6 @@ class DatabaseManager:
 
             bucket_to_clear = self.bucket
             start = "1970-01-01T00:00:00Z"
-            # Using datetime.now() with timezone.utc to get current UTC time
-            # Formatting manually to include milliseconds, ensuring compliance with RFC3339
             stop = datetime.now(timezone.utc).strftime('%Y-%m-%dT%H:%M:%S.%f')[:-3] + 'Z'
 
             url = f"{self.client.url}/api/v2/delete?org={self.org}&bucket={bucket_to_clear}"
@@ -330,8 +288,3 @@ class DatabaseManager:
         except Exception as e:
             print(f"General exception occurred: {e}")
             return False
-
-
-
-
-
